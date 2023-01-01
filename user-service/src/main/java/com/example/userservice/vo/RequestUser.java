de6d8d541378c0343aa47fd5c09b11eecf388d29
sungsanghyun <sos9210@naver.com>
2023-01-01 23:08:23 +0900

    스프링시큐리티적용, userService 사용자등록

---
+++ user-service/src/main/java/com/example/userservice/vo/RequestUser.java
@@ -0,0 +1,25 @@
+package com.example.userservice.vo;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RequestUser {
+
+    @NotNull(message = "Email cannot be null")
+    @Size(min = 2, message = "Email not be less than 2 characters")
+    @Email
+    private String email;
+
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 2, message = "Name not be less than 2 characters")
+    private String name;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, message = "Password must be equal or grater than 8 characters")
+    private String pwd;
+
+}
