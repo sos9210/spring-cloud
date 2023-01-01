d4acd2e9c3fd7928eb8245ae53535e8087a48ab0
sungsanghyun <sos9215@gmail.com>
2023-01-01 23:08:23 +0900

    스프링시큐리티적용, userService 사용자등록

---
+++ user-service/src/main/java/com/example/userservice/vo/ResponseUser.java
@@ -0,0 +1,13 @@
+package com.example.userservice.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ResponseUser {
+
+    private String email;
+    private String name;
+    private String userId;
+
+}
