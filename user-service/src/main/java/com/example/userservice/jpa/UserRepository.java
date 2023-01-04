e3212b88140b0afce1bd16948e2e8bb5da175935
sungsanghyun <sos9215@gmail.com>
2023-01-05 00:37:16 +0900

    23/01/04 - 시큐리티 설정코드 작성

--- user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity,String> {
     Optional<UserEntity> findByUserId(String userId);
+    Optional<UserEntity> findByEmail(String email);
 }
--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,20 +1,57 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
+import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity{
+public class WebSecurity {
+    private AuthenticationConfiguration authenticationConfiguration;
+    private BCryptPasswordEncoder passwordEncoder;
+    private UserService userService;
+    private Environment env;
+    public WebSecurity(Environment env, AuthenticationConfiguration authenticationConfiguration, BCryptPasswordEncoder passwordEncoder, UserService userService) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+        this.env = env;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests().anyRequest().permitAll();
+        //http.authorizeHttpRequests().anyRequest().permitAll();
+        http.authorizeHttpRequests()
+                .requestMatchers("/**")
+                .permitAll()
+                .and()
+                .addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
         return http.build();
     }
+    @Bean
+    public AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationConfiguration.getAuthenticationManager());
+
+        return authenticationFilter;
+    }
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+        return authenticationManager;
+    }
+
 }
